92f4baae3f114feefdc3ac269bdff482fa68c8b9
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-06-18 15:50:14 +0200

    Fix: pycodestyle

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,10 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawns wait_random n times with max_delay and returns list of delays in ascending order."""
+    """
+    Spawns wait_random n times with max_delay and returns list
+        of delays in ascending order.
+    """
     delays = []
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     for task in asyncio.as_completed(tasks):
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,7 +6,10 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawns task_wait_random n times and returns list of delays in ascending order."""
+    """
+    Spawns task_wait_random n times and returns list
+    of delays in ascending order.
+    """
     delays = []
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     for task in asyncio.as_completed(tasks):
